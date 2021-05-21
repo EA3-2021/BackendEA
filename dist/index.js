0da5ab8bf5c4a514aa7363491c48e117b2e5d494
Her19mo61so! <meritxellhermoso@gmail.com>
2021-05-21 20:28:27 +0200

    chat funcionando

--- dist/index.js
+++ dist/index.js
@@ -7,5 +7,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app")); //app exportada en app.ts
 //Ejecutamos la conexion a la BBDD antes de escuchar al server
 require("./database");
-app_1.default.listen(app_1.default.get('port')); //Recuperamos puerto de app.ts
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const httpServer = http_1.createServer(app_1.default);
+const io = new socket_io_1.Server(httpServer, { cors: { origin: '*' } });
+io.on("connection", (socket) => {
+    console.log("nova connexió");
+    //console.log(socket);
+    /*
+        socket.on('disconnect', function(){
+            io.emit('users-changed', {user: socket.username, event: 'left'});
+        });
+    */
+    socket.on('set-name', (name) => {
+        console.log('SER NAME: ', name);
+        // socket.username = name;
+        io.emit('users-changed', { user: name, event: 'joined' });
+    });
+    socket.on('send-message', (message) => {
+        io.emit('message', { msg: message.text, createdAt: new Date() }); //, user: socket.username,
+    });
+});
+httpServer.listen(app_1.default.get('port')); //Recuperamos puerto de app.ts
 console.log('Server in port', app_1.default.get('port'));
